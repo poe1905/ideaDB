51f799a3f69e1751313cf9a046251e3ed473ba9a
李一 <593049631@qq.com>
2020-02-12 09:59:41 +0800

    初始化目录结构-get 请求

---
+++ app.js
@@ -0,0 +1,29 @@
+const Koa = require('koa');
+
+// 注意require('koa-router')返回的是函数:
+const router = require('koa-router')();
+
+const app = new Koa();
+
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+});
+
+// add url-route:
+router.get('/hello/:name/ab', async (ctx, next) => {
+  console.log(ctx.query);
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1> `;
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = '<h1>Index</h1>';
+});
+
+// add router middleware:
+app.use(router.routes());
+
+app.listen(3000);
+console.log('app started at port 3000...');
